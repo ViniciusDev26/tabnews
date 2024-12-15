9b2de2e34dcc8bfaa79bf004e792df61c5b3e30e
Carlos Vinicius <viniciusdev.26@gmail.com>
2024-12-15 18:47:51 -0300

    add `database.ts` and configure `.env` files

---
+++ infra/database.ts
@@ -0,0 +1,20 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+	const client = new Client({
+		host: process.env.POSTGRES_HOST,
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		port: Number(process.env.POSTGRES_PORT),
+		database: process.env.POSTGRES_DB,
+	});
+	await client.connect();
+	const result = await client.query(queryObject);
+	await client.end();
+
+	return result;
+}
+
+export default {
+	query,
+};
--- pages/api/v1/status.ts
+++ pages/api/v1/status.ts
@@ -1,8 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import database from "../../../infra/database";
 
-export default function status(
-  request: NextApiRequest,
-  response: NextApiResponse
+export default async function status(
+	request: NextApiRequest,
+	response: NextApiResponse,
 ) {
-  return response.status(200).json({ status: 'ok' })
+	const result = await database.query("SELECT NOW()");
+	console.log(result);
+	return response.status(200).json({ status: "ok" });
 }
